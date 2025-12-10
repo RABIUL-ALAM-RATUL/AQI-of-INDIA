2942c7232b95cfe3689674f40fb707707deac57d
MD RABIUL ALAM <mrabiul.alam96@gmail.com>
2025-12-10 04:24:46 +0000

    Remove unnecessary writefile directive

--- app.py
+++ app.py
@@ -4,7 +4,6 @@
 # STUDENT ID: ST20316895
 # ============================================================================
 
-%%writefile app.py
 import streamlit as st
 import pandas as pd
 import numpy as np
